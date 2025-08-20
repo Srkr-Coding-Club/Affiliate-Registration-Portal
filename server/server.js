b50ae397d0c6b0fdba8942c696eaa59709c5962e
SRKR CODING CLUB <srkrcodingclub@gmail.com>
2025-08-20 09:22:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://affiliate-registration-portal.vercel.app',
         credentials: true
     }
 ));
